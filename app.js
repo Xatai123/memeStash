3573eed93ff5e49e9264213c1ec74061c21f54e5
Xatai123 <xataialiyev5@gmail.com>
2019-08-03 16:51:43 +0400

    testing

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var express = require("express"),
-  app = express(),
-  bodyParser = require("body-parser"),
-  mongoose = require("mongoose"),
-  flash = require("connect-flash"),
-  passport = require("passport"),
-  LocalStrategy = require("passport-local"),
-  methodOverride = require("method-override"),
-  User = require("./models/user"),
+const express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    flash = require("connect-flash"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    User = require("./models/user"),
 
-var commentRoutes = require("./routes/comments"),
-  campgroundRoutes = require("./routes/campgrounds"),
-  authRoutes = require("./routes/auth");
+const commentRoutes = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    authRoutes = require("./routes/auth");
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {
-  useNewUrlParser: true
+    useNewUrlParser: true
 });
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "ejs");
@@ -24,11 +24,11 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 app.use(
-  require("express-session")({
-    secret: "this is a secret",
-    resave: false,
-    saveUninitialized: false
-  })
+    require("express-session")({
+        secret: "this is a secret",
+        resave: false,
+        saveUninitialized: false
+    })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -37,10 +37,10 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use(function (req, res, next) {
-  res.locals.currentUser = req.user;
-  res.locals.error = req.flash("error");
-  res.locals.success = req.flash("success");
-  next();
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
 });
 
 app.use(authRoutes);
@@ -48,5 +48,5 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function () {
-  console.log("Yelpcamp started");
+    console.log("Yelpcamp started");
 });
