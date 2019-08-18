ce5061f27c2822ed4c09a167aa8781ad6cbc4033
Xatai123 <xataialiyev5@gmail.com>
2019-08-18 17:49:40 +0400

    added user profiles

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
+var Meme = require("../models/meme");
 
 router.get("/", function (req, res) {
 	res.render("landing");
@@ -13,8 +14,14 @@ router.get("/register", function (req, res) {
 });
 
 router.post("/register", function (req, res) {
-	var newUser = new User({ username: req.body.username });
-	if( req.body.adminCode === "MemesAreLife"){
+	var newUser = new User({
+		username: req.body.username,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		avatar: req.body.avatar
+	});
+	if (req.body.adminCode === "MemesAreLife") {
 		newUser.isAdmin = true;
 	}
 	User.register(newUser, req.body.password, function (err, user) {
@@ -23,7 +30,7 @@ router.post("/register", function (req, res) {
 			return res.redirect("register");
 		}
 		passport.authenticate("local")(req, res, function () {
-			req.flash("success", "Welcome to Xatai's Meme website ," + user.username);
+			req.flash("success", "Welcome to Xatai's Meme website, " + user.username);
 			res.redirect("/memes");
 		});
 	});
@@ -49,6 +56,26 @@ router.get("/logout", function (req, res) {
 	req.logout();
 	req.flash("error", "Logged you out");
 	res.redirect("/memes");
+});
+
+
+// user profile
+router.get("/users/:id", function (req, res) {
+	User.findById(req.params.id, function (err, foundUser) {
+		if (err) {
+			req.flash("error", err.message);
+			res.redirect("/")
+		} else {
+			Meme.find().where('author.id').equals(foundUser._id).exec(function (err, memes) {
+				if (err) {
+					req.flash("error", err.message);
+					res.redirect("/")
+				} else {
+					res.render("users/show", { user: foundUser, memes: memes });
+				}
+			})
+		}
+	});
 });
 
 module.exports = router;
